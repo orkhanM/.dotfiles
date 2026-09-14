8953772626a15c69011fd7abfd3c200b92b9e811
Orkhan Mamedov <orkhan.mamedov.7@gmail.com>
2026-09-14 16:19:37 -0400

    Don't add brew formula internals to PATH

--- zsh/.config/zsh/osx.zsh
+++ zsh/.config/zsh/osx.zsh
@@ -7,35 +7,14 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 # fi
 eval "$(fnm env)"
 
-# creates a ~/.path file with some osx specific paths if it doesn't exist
-if [ ! -e $HOME/.path ]; then
-  echo -n "
-/opt/homebrew/bin
-/usr/local/bin
-/usr/local/sbin
-$HOME/.local/bin
-" >$HOME/.path
-
-  for i in $(echo -n "
-/usr/local/opt
-/opt/homebrew/opt
-"); do
-    if [ -d $i ]; then
-      find -L $i -type d -name '*bin' |
-        grep -v '/node_modules/' |
-        grep -v '/gems/' |
-        sort \
-          >>$HOME/.path
-    fi
-  done
+brew_bins=(
+  /opt/homebrew/opt/*/libexec/gnubin(N)
+  /opt/homebrew/opt/*/bin(N)
+  /opt/homebrew/opt/*/sbin(N)
+)
+if ((${#brew_bins})); then
+  PATH="${(j.:.)brew_bins}:$PATH"
 fi
+unset brew_bins
 
-# We add all the paths from ~/.path to the PATH variable
-if [ -e $HOME/.path ]; then
-  for i in $(nl $HOME/.path | sort -nr | cut -f 2-); do
-    if [ -e $i ]; then
-      PATH=$i:$PATH
-    fi
-  done
-fi
 export PATH
